1beb3f05f26674055c7079dc2c0d6ee3ca8031d3
Francisco <drmalito.weid@gmail.com>
2023-11-05 22:24:44 -0400

    jk

--- j.py
+++ j.py
@@ -9,3 +9,13 @@ personas2 = [nombre.capitalize() + " " + apellido.capitalize() for nombre, apell
 
 print(personas)
 print(personas2)
+
+# ...
+
+i = list(range(1, 100))
+
+impares = [x for x in i if x % 2 != 0]
+pares = [x for x in i if x % 2 == 0]
+
+print(impares)
+print(pares)
