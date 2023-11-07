2c2f598588ef41d51ef1278defe8b334a01fdd8a
Francisco <drmalito.weid@gmail.com>
2023-11-06 20:17:49 -0400

    cl

--- c2.py
+++ c2.py
@@ -1,3 +1,4 @@
+import os
 import sqlite3
 
 conn = sqlite3.connect('c.db')
@@ -6,7 +7,7 @@ c = conn.cursor()
 c.execute("CREATE TABLE if not exists users (id INTEGER, name TEXT, email TEXT, age INTEGER)")
 
 
-def get_data():
+def get_data() -> list[dict] | list:
     c.execute("SELECT id, name, email, age FROM users")
     users_data = c.fetchall()
     real_users = []
@@ -22,49 +23,49 @@ def get_data():
     return real_users
 
 
-def insert_data(id_user: int, name: str, email: str, age: int):
-    c.execute(f"INSERT INTO users VALUES ({id_user}, '{name}', '{email}', {age})")
-    conn.commit()
-
-
-def update_data(id_user: int, name: str, email: str, age: int):
-    c.execute(f"UPDATE users SET name = '{name}', email = '{email}', age = {age} WHERE id = {id_user}")
-    conn.commit()
-
-
-def delete_data(id_user: int):
-    c.execute(f"DELETE FROM users WHERE id = {id_user}")
-    conn.commit()
+def get_option() -> int:
+    while True:
+        try:
+            option = int(input("Select an option: "))
+            return option
+        except ValueError:
+            print("Invalid option")
 
 
 while True:
-    print("1. Get data")
-    print("2. Insert data")
-    print("3. Update data")
-    print("4. Delete data")
-    print("5. Exit")
-    option = int(input("Select an option: "))
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-    match option:
+    print("1. Get users")
+    print("2. Insert user")
+    print("3. Update user")
+    print("4. Delete user")
+    print("5. Exit")
+    op: int = get_option()
+
+    match op:
         case 1:
             users = get_data()
             for user in users:
                 print(f"({user['id']}) {user['name']}: {user['email']} - {user['age']}")
+            input("Press enter to continue...")
         case 2:
             id_user = int(input("Enter id: "))
             name = input("Enter name: ")
             email = input("Enter email: ")
             age = int(input("Enter age: "))
-            insert_data(id_user, name, email, age)
+            c.execute(f"INSERT INTO users VALUES ({id_user}, '{name}', '{email}', {age})")
+            conn.commit()
         case 3:
             id_user = int(input("Enter id: "))
             name = input("Enter name: ")
             email = input("Enter email: ")
             age = int(input("Enter age: "))
-            update_data(id_user, name, email, age)
+            c.execute(f"UPDATE users SET name = '{name}', email = '{email}', age = {age} WHERE id = {id_user}")
+            conn.commit()
         case 4:
             id_user = int(input("Enter id: "))
-            delete_data(id_user)
+            c.execute(f"DELETE FROM users WHERE id = {id_user}")
+            conn.commit()
         case 5:
             break
         case _:
--- l.py
+++ l.py
@@ -1,1 +1,16 @@
-# L
+# print(bytes([0x41, 0x42, 0x43, 0x44, 0x45]))
+
+import datetime
+
+# from time import sleep
+
+print(datetime.datetime.now())
+print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"))
+print(datetime.datetime.now().strftime("%Y-%m-%d %I:%M:%S.%f %p"))
+print(datetime.datetime.now().strftime("%Y-%m-%d %I:%M:%S.%f %p %Z"))
+print(datetime.datetime.now().strftime("%Y-%m-%d %I:%M:%S.%f %p %Z %z"))
+
+# while True:
+#     print(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S %p"))
+#     sleep(1)
